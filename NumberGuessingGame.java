816d326da678ec3ddb9f29e8b58a92e153bc2338
Smj3753 <142374966+Smj3753@users.noreply.github.com>
2023-09-07 18:42:52 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int maxAttempts = 10;
+        int rounds = 0;
+        int score = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between " + minRange + " and " + maxRange + ".");
+
+        boolean playAgain = true;
+        while (playAgain) {
+            int targetNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed the correct number (" + targetNumber + ") in " + attempts + " attempts.");
+                    score += maxAttempts - attempts + 1;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
+            }
+
+            rounds++;
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainChoice = scanner.next().toLowerCase();
+            if (!playAgainChoice.equals("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Thanks for playing!");
+        System.out.println("Rounds played: " + rounds);
+        System.out.println("Total score: " + score);
+
+        scanner.close();
+    }
+}
